59b6e40d877a44698e8c69ec263800e492aca507
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-11-09 22:04:05 +0000

    MUI palette configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,35 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Skeleton from '@mui/material/Skeleton';
-import { ChartContainer } from '@mui/x-charts';
-import { LineChart, LinePlot, MarkPlot } from '@mui/x-charts/LineChart';
-import { PieChart } from '@mui/x-charts/PieChart';
-import GridComponent from './GridComponent';
-import CardComponent from './components/CardComponent';
-import LineChartComponent from './components/LineChartComponent';
-// import BasicLineChart from './components/BasicLineChart';
-
-
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const xLabels = [
-  'Page A',
-  'Page B',
-  'Page C',
-  'Page D',
-  'Page E',
-  'Page F',
-  'Page G',
-];
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import { PieChart } from "@mui/x-charts/PieChart";
+import CardComponent from "./components/CardComponent";
+import LineChartComponent from "./components/LineChartComponent";
+import { Button } from "@mui/material";
 
 export default function App() {
   return (
     <Container maxWidth="sm">
-      <div style={{'display': 'flex'}}>
+      <h1>Dashboard</h1>
+      <div style={{ display: "flex" }}>
         <Box sx={{ my: 8 }}>
-          <h2 style={{'color':'grey'}}>Overview</h2>
-          <div style={{'display': 'flex'}}>
-            {/* <Skeleton variant="rounded" width={210} height={60} /> */}
-            <CardComponent/>
-            <CardComponent/>
-            <CardComponent/>
+          <h2>Overview</h2>
+          <div style={{ display: "flex" }}>
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
           </div>
-          <h2 style={{'color':'grey'}}>Details</h2>
-          <div style={{'display': 'flex'}}>
-            <LineChartComponent/>
-
+          <h2>Details</h2>
+          <div style={{ display: "flex" }}>
+            <LineChartComponent />
           </div>
-          
 
-          <div  style={{'display': 'flex'}}>
-
+          <div style={{ display: "flex" }}>
             <PieChart
               series={[
                 {
                   data: [
-                    { id: 0, value: 10, label: 'series A' },
-                    { id: 1, value: 15, label: 'series B' },
-                    { id: 2, value: 20, label: 'series C' },
+                    { id: 0, value: 10, label: "series A" },
+                    { id: 1, value: 15, label: "series B" },
+                    { id: 2, value: 20, label: "series C" },
                   ],
                 },
               ]}
@@ -61,8 +38,12 @@ export default function App() {
             />
           </div>
         </Box>
-
       </div>
+
+      <Button variant="contained">Hi</Button>
+      <Button variant="contained" color="secondary">
+        Hi
+      </Button>
 
     </Container>
   );
--- src/theme.ts
+++ src/theme.ts
@@ -1,23 +1,45 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-// A custom theme for this app
-const theme = createTheme({
+let theme = createTheme({
   palette: {
+    mode: "dark",
     primary: {
-      main: '#556cd6',
+      main: "#6D95CA",
     },
     secondary: {
-      main: '#19857b',
+      main: "#E74C3C",
     },
     error: {
-      main: red.A400,
-    }
-    // ,
-    // background: {
-    //   default: '#000000'
-    // }
+      main: "#FF5454",
+    },
+    success: {
+      main: "#54FF98",
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#CCCCCC"
+    },
+    background: {
+      default: "#0E1520",
+      paper: "#0E1520",
+    },
+  },
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
   },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
