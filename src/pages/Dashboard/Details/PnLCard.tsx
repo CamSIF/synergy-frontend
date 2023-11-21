e66572b8388008abecf88ca429d514a8599f8853
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-11-21 14:49:38 +0000

    Mock data demo

--- src/pages/Dashboard/Details/PnLCard.tsx
+++ src/pages/Dashboard/Details/PnLCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DetailsCard from "src/pages/Dashboard/Details/DetailsCard";
+import LineChartComponent from "src/components/LineChartComponent";
 
 interface PnLCardProps {
   title: string;
@@ -8,7 +9,7 @@ interface PnLCardProps {
 export const PnLCard: React.FC<PnLCardProps> = ({ title }) => {
   return (
     <DetailsCard title={title} responsive={{ xs: 12, sm: 12, md: 12 }}>
-      <div>PnL card</div>
+      <LineChartComponent xAxis={[{ data: [1, 2, 3] }]} yAxis={[1, 4, 9]} />
     </DetailsCard>
   );
 };
