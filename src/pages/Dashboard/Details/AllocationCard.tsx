e4e11c9a9fb63d808c2b30d1df56a89ccdf93687
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-11-21 13:20:16 +0000

    Add mock demo

--- src/pages/Dashboard/Details/AllocationCard.tsx
+++ src/pages/Dashboard/Details/AllocationCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DetailsCard from "src/pages/Dashboard/Details/DetailsCard";
+import PieChartComponent from "src/components/PieChartComponent";
 
 interface AllocationCardProps {
   title: string;
@@ -8,7 +9,17 @@ interface AllocationCardProps {
 export const AllocationCard: React.FC<AllocationCardProps> = ({ title }) => {
   return (
     <DetailsCard title={title} responsive={{ xs: 12, sm: 12, md: 6 }}>
-      Allocation card
+      <PieChartComponent
+        title="Test"
+        data={[
+          { id: 0, value: 10, label: "series A" },
+          { id: 1, value: 15, label: "series B" },
+          { id: 2, value: 20, label: "series C" },
+        ]}
+        percentage={false}
+        width={400}
+        height={200}
+      />
     </DetailsCard>
   );
 };
