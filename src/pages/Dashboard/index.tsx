ab6f4be5d0fca2105c52146a2b19a258369d8954
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2023-11-26 16:03:08 +0000

    Create dropdown component and put on dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
 import Wrapper from "src/components/Wrapper";
 import Header1 from "src/components/Title/Header1";
+import Dropdown from "src/components/DataInput/Dropdown";
+import FlexContainer from "src/components/FlexBox/FlexContainer";
+import FlexItem from "src/components/FlexBox/FlexItem";
 
 import Overview from "src/pages/Dashboard/Overview";
 import Details from "src/pages/Dashboard/Details";
 
+import { DropdownItem } from "src/types/DataInput";
+
+import { styled } from "@mui/material";
+
+const StyledDiv = styled("div")({
+  height: "80px",
+});
+
 export const Dashboard: React.FC<{}> = () => {
+  const [fund, setFund] = useState("");
+  const fundItems: DropdownItem[] = [
+    {
+      label: "Main",
+      value: "camsif-main",
+    },
+  ];
+
   return (
     <Wrapper>
-      <Header1>Dashboard</Header1>
+      <FlexContainer>
+        <FlexItem responsive={{ xs: 12, sm: 6, md: 8 }}>
+          <Header1>Dashboard</Header1>
+        </FlexItem>
+        <FlexItem responsive={{ xs: 12, sm: 6, md: 4 }}>
+          <StyledDiv>
+            <Dropdown
+              state={fund}
+              setState={setFund}
+              label="Fund"
+              items={fundItems}
+              minWidth={150}
+            />
+          </StyledDiv>
+        </FlexItem>
+      </FlexContainer>
       <Overview />
       <Details />
     </Wrapper>
