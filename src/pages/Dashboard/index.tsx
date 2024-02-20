1b223dc3a6038fc4a142745e9fc95e40e605b390
ChloeYiu <chloe.yiu.yee.man@gmail.com>
2024-02-20 01:38:41 +0000

    First commit of two dropdowns

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Wrapper from "src/components/Wrapper";
 import Header1 from "src/components/Title/Header1";
@@ -21,37 +21,60 @@ import { styled } from "@mui/material";
 
 const queryParameters = new URLSearchParams(window.location.search);
 const defaultFund = "main";
+const defaultAccount = "camsif";
 const initialFund = queryParameters.get("fund") ?? defaultFund;
-const url = `${process.env.REACT_APP_API_URL}api/get_account_funds`;
+const initialAccount = queryParameters.get("account") ?? defaultAccount;
+const url = `${process.env.REACT_APP_API_URL}api/get_all_account_funds`;
 
 const StyledDiv = styled("div")({
   height: "80px",
 });
 
+let accountFundMap: {[key: string]: string[]} | undefined = undefined;
+
 export const Dashboard: React.FC<{}> = () => {
   const [fund, setFund] = useState(initialFund);
   const [fundList, setFundList] = useState<string[] | undefined>(undefined);
-  const account = useContext(AccountContext);
+  const [account, setAccount] = useState(initialAccount);
+  const [accountList, setAccountList] = useState<string[] | undefined>(
+    undefined
+  );
 
-  const handleChange = (event: DropdownEvent) => {
+  const handleFundChange = (event: DropdownEvent) => {
     const newFund = event.target.value as string;
     setFund(newFund);
-    window.history.replaceState(null, "", `?fund=${newFund}`);
+    window.history.replaceState(
+      null,
+      "",
+      `?account=${account}&fund=${newFund}`
+    );
+  };
+
+  const handleAccountChange = (event: DropdownEvent) => {
+    const newAccount = event.target.value as string;
+    const newFundList = accountFundMap![newAccount];
+    setAccount(newAccount);
+    setFundList(newFundList);
+    setFund(newFundList[0]);
+    window.history.replaceState(null, "", `?account=${newAccount}`);
   };
 
   useEffect(() => {
-    const req = new FormData();
-    req.append("account", account);
     axios
-      .post(url, req)
+      .post(url)
       .then((response) => {
-        const list = response.data.funds;
-        setFundList(list);
-        if (!list.includes(fund)) {
+        const resAccountFundMap = response.data;
+        const resAccountList = Object.keys(resAccountFundMap);
+        const resFundList = resAccountFundMap[account];
+        accountFundMap = resAccountFundMap
+        setAccountList(resAccountList);
+        setFundList(resFundList);
+        if (!resAccountList.includes(account) || !resFundList.includes(fund)) {
+          setAccount(defaultAccount);
           setFund(defaultFund);
           enqueueAlertStack(
-            `Invalid link with fund parameter '${fund}'. Display default portfolio '${valueToLabel(
-              account
+            `Invalid link with account '${account}' and fund '${fund}'. Display default portfolio '${valueToLabel(
+              defaultAccount
             )} ${valueToLabel(defaultFund)}'`,
             "error"
           );
@@ -68,15 +91,40 @@ export const Dashboard: React.FC<{}> = () => {
   }, [account, fund]);
 
   return (
-    <AccountContext.Provider value="camsif">
+    <AccountContext.Provider value={account}>
       <FundContext.Provider value={fund}>
         <Wrapper>
           <AlertStack />
           <FlexContainer>
-            <FlexItem responsive={{ xs: 12, sm: 6, md: 8 }}>
+            <FlexItem responsive={{ xs: 12, sm: 12, md: 4, lg: 6 }}>
               <Header1>Dashboard</Header1>
             </FlexItem>
-            <FlexItem responsive={{ xs: 12, sm: 6, md: 4 }}>
+            <FlexItem responsive={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+              <StyledDiv>
+                {accountList === undefined ? (
+                  <DropdownSkeleton
+                    initialItem={{
+                      label: capitalizeWord(initialAccount),
+                      value: initialAccount,
+                    }}
+                    label="Account"
+                    minWidth={150}
+                  />
+                ) : (
+                  <Dropdown
+                    state={account}
+                    handleChange={handleAccountChange}
+                    label="Account"
+                    items={accountList.map((account) => ({
+                      label: valueToLabel(account),
+                      value: account,
+                    }))}
+                    minWidth={150}
+                  />
+                )}
+              </StyledDiv>
+            </FlexItem>
+            <FlexItem responsive={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
               <StyledDiv>
                 {fundList === undefined ? (
                   <DropdownSkeleton
@@ -90,7 +138,7 @@ export const Dashboard: React.FC<{}> = () => {
                 ) : (
                   <Dropdown
                     state={fund}
-                    handleChange={handleChange}
+                    handleChange={handleFundChange}
                     label="Fund"
                     items={fundList.map((fund) => ({
                       label: valueToLabel(fund),
